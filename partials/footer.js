4e88e7903571731f9479fb9154f633968773494d
Max Fillline <themaxfillline@gmail.com>
2026-05-16 18:39:20 -0400

    reimplement partials system

---
+++ partials/footer.js
@@ -0,0 +1,3 @@
+document.querySelector('footer').innerHTML = `
+    <p>&copy; 2026 Max Fillline. All rights reserved. Don't steal my shit, please.</p>
+    `
---
+++ partials/header.js
@@ -0,0 +1,4 @@
+document.querySelector('header').innerHTML = `
+    <h1 ><a href="/">Max Fillline</a></h1>
+    <h2>${document.querySelector('#h2-data').innerHTML}</h2>
+    `
